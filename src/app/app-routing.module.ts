d92118d582549943b1492f1890ab74451f612465
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-05 08:01:17 +0200

    feat(app): :sparkles: add routes pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomePageComponent } from "./shared/pages/home-page/home-page.component";
-import { AboutPageComponent } from "./shared/pages/about-page/about-page.component";
-
+import { NgModule } from '@angular/core'
+import { RouterModule, type Routes } from '@angular/router'
+import { HomePageComponent } from './shared/pages/home-page/home-page.component'
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component'
+import { ContactComponent } from './shared/pages/contact/contact.component'
 
 const routes: Routes = [{
-  path: 'home',
+  path: '',
   component: HomePageComponent
 }, {
   path: 'about',
   component: AboutPageComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
 }, {
   path: '**',
   component: HomePageComponent
-}];
-
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
