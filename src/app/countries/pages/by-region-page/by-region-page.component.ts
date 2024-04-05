92d3458927c17cdb9d07d259ac4736481cdce0c9
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-05 15:20:01 +0200

    feat(countries): :sparkles: change the search method in region-page for limited buttons options

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core'
 import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'country-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ''
 })
+
 export class ByRegionPageComponent {
   countries: Country[] = [];
+  regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   title: string = 'región';
   isLoading: boolean = false;
+  selectedRegion?: Region;
   constructor(private countriesService: CountryService) { }
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
+    this.selectedRegion = term;
     this.isLoading = true;
     this.countriesService.searchRegion(term).subscribe(countries => {
       this.countries = countries;
